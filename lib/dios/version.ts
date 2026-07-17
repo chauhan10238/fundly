2c515266cebb59069742f8473896af3364387fbc
chauhan10238 <87007664+chauhan10238@users.noreply.github.com>
2026-07-17 13:22:38 +1000

    Update version.ts

--- lib/dios/version.ts
+++ lib/dios/version.ts
@@ -1,2 +1,2 @@
-export const DIOS_VERSION = "1.2.0"
-export const DIOS_RELEASE_NAME = "Live Portfolio Prices"
+export const DIOS_VERSION = "1.3.0"
+export const DIOS_RELEASE_NAME = "Live Analyse Decision Engine"
